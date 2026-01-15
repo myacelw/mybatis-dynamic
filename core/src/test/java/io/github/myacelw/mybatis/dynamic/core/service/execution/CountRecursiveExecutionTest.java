97963af8ad79d58bfa6267527c1d5807b955c653
liuwei <liuwei@yunlizhihui.com>
2026-01-15 12:04:12 +0800

    test(execution): Add integration test for CountRecursiveExecution

---
+++ core/src/test/java/io/github/myacelw/mybatis/dynamic/core/service/execution/CountRecursiveExecutionTest.java
@@ -0,0 +1,25 @@
+package io.github.myacelw.mybatis.dynamic.core.service.execution;
+
+import io.github.myacelw.mybatis.dynamic.core.metadata.query.condition.SimpleCondition;
+import io.github.myacelw.mybatis.dynamic.core.service.DataManager;
+import io.github.myacelw.mybatis.dynamic.core.service.command.CountRecursiveCommand;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CountRecursiveExecutionTest extends BaseExecutionTest {
+
+    @Test
+    void exec_CountRecursive() {
+        DataManager<String> dataManager = getDataManager("Department");
+        CountRecursiveExecution<String> execution = new CountRecursiveExecution<>();
+
+        CountRecursiveCommand command = new CountRecursiveCommand();
+        command.setInitNodeCondition(SimpleCondition.eq("id", "d1"));
+        command.setRecursiveDown(true);
+
+        Long count = execution.exec(command, dataManager);
+
+        assertEquals(4, count);
+    }
+}
