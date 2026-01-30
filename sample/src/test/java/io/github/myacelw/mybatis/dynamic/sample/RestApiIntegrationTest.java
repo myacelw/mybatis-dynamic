4d14cb49bec450ad800ea858b54238093195ffee
liuwei <liuwei@yunlizhihui.com>
2026-01-30 18:04:22 +0800

    兼容性改造

--- sample/src/test/java/io/github/myacelw/mybatis/dynamic/sample/RestApiIntegrationTest.java
+++ sample/src/test/java/io/github/myacelw/mybatis/dynamic/sample/RestApiIntegrationTest.java
@@ -1,13 +1,15 @@
 package io.github.myacelw.mybatis.dynamic.sample;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,14 +18,20 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 public class RestApiIntegrationTest {
 
-    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
     private ObjectMapper objectMapper;
+
+    @BeforeEach
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
 
     @Test
     public void testDicCrud() throws Exception {
